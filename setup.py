17a963525bed6dc97ed70ee788d854cac9ffaae2
Dan habib <57772926+Snowy-27@users.noreply.github.com>
2020-09-07 19:51:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='notification',
-    version='0.0.1',
-    description='My private package from private github repo',
-    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
-    author='Raphael Schubert',
-    author_email='raphael.schubert@digitalbankscorp.com',
+    version='1.0',
+    description='With this package you can send notifications to your android phone via ifttt.',
+    url='https://github.com/Snowy-27/Notification'
+    author='Dan Habib',
+    author_email='dan.habib@yahoo.fr',
     license='unlicense',
     packages=['notification'],
     zip_safe=False
