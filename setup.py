87034b1f6d2bc99611f8ca91b0dc7fa0af99d977
Dan habib <57772926+Snowy-27@users.noreply.github.com>
2020-09-07 19:39:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='Dan',
+    name='notification',
     version='0.0.1',
     description='My private package from private github repo',
     url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
